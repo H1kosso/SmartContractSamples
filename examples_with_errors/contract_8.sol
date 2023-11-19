8923b91389540d8824ae0a30074513386ca5011a
Dawid_W <purchowa@gmail.com>
2023-11-19 14:21:01 +0100

    upd: merge many contracts into one

--- examples_with_errors/contract_8.sol
+++ examples_with_errors/contract_8.sol
@@ -9,16 +9,24 @@ Potencjalne zagrożenia:
 // solc version 0.8.0
 pragma solidity ^0.8.0;
 
-contract Delegate {
+contract ManyMistakes {
+    uint public n;
+    address public sender;
+    address payable public owner;
+    address public implementation;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    // ---
+
     function delegatecallSetN(address _e, uint _n) public {
         (bool result,) = _e.delegatecall(abi.encodeWithSignature("setN(uint256)", _n));
         require(result);
     }
-}
 
-contract SetGet {
-    uint public n;
-    address public sender;
+    // ---
 
     function setN(uint _n) public {
         n = _n;
@@ -28,24 +36,16 @@ contract SetGet {
     function getN() public view returns (uint) {
         return n;
     }
-}
 
-contract EtherTransfer {
-    address payable public owner;
-
-    constructor() {
-        owner = payable(msg.sender);
-    }
+    // ---
 
     function sendEther(address payable _to, uint _amount) public {
         require(msg.sender == owner, "Only owner can send ether");
         (bool sent,) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
     }
-}
 
-contract Upgradeable {
-    address public implementation;
+    // ---
 
     function upgradeTo(address _newImplementation) public {
         implementation = _newImplementation;
